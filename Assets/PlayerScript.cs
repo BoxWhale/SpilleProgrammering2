60b2cf7224b827220dee3fe8ace82d989a7eb0b7
Happy_Majima <62021893+BoxWhale@users.noreply.github.com>
2025-04-16 21:36:25 +0200

    LightShadowV2

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,99 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody _rb;
+    public float speed;
+    public float acceleration;
+    public float mouseSensitivity;
+    public float yMouseOffsetSensitivityFactor;
+    
+    private PlayerInput playerInput;
+    
+    private InputAction moveAction;
+    public Vector2 movement;
+    
+    public InputAction lookAction;
+    public Vector2 look;
+
+    public Vector3 offset;
+    public float viewOffset;
+    [Tooltip("x = min\ny = max")]
+    public Vector2 verticalClamp;
+    public GameObject sun;
+    
+    private void OnEnable()
+    {
+        sun = GameObject.Find("Sun");
+        _rb = gameObject.GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+        lookAction = playerInput.actions["Player/Look"];
+        moveAction = playerInput.actions["Player/Move"];
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+    }
+    
+    void LateUpdate()
+    {
+        ShadowDetection();
+        CameraRotation();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+        Debug.Log(moveAction.ReadValue<Vector2>());
+    }
+
+    void CameraRotation()
+    {
+        Vector2 look = lookAction.ReadValue<Vector2>();
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + look.x*1/mouseSensitivity, 0);
+        offset = new(offset.x, Mathf.Clamp(offset.y + -look.y*((1/mouseSensitivity)/yMouseOffsetSensitivityFactor),verticalClamp.x,verticalClamp.y), offset.z);
+        
+        float r = new Vector3(offset.x, offset.y, offset.z).magnitude;
+        float theta = Mathf.Acos(offset.y / r);
+        float phi = -transform.eulerAngles.y * Mathf.Deg2Rad;
+
+        float x = -(r * Mathf.Sin(theta) * Mathf.Cos(phi));
+        float y = r * Mathf.Cos(theta);
+        float z = -(r * Mathf.Sin(theta) * Mathf.Sin(phi));
+            
+
+        Camera.main.transform.position = transform.position + new Vector3(x, y, z);
+        Camera.main.transform.LookAt(transform.position + Vector3.up * viewOffset);
+    }
+
+    void PlayerMovement()
+    {
+        movement = moveAction.ReadValue<Vector2>();
+        Vector3 VerticalVelocity = new Vector3(0, _rb.linearVelocity.y, 0);
+        Vector3 HorizontalVelocity = new Vector3(movement.y, 0, -movement.x)*speed;
+        
+        Vector3 TargetVelocity = VerticalVelocity + transform.TransformDirection(HorizontalVelocity);
+        _rb.linearVelocity = Vector3.Lerp(_rb.linearVelocity, TargetVelocity, acceleration*Time.deltaTime);
+    }
+
+    void ShadowDetection()
+    {
+        Ray ray = new Ray(transform.position-Vector3.up*transform.localScale.y/2, sun.transform.rotation*Vector3.back*100f);
+        if (Physics.Raycast(ray, out RaycastHit hit, 100, LayerMask.GetMask("Shadow")))
+        {
+            //In shadow
+            Debug.DrawRay(ray.origin, ray.direction*1000, Color.green);
+        }
+        else
+        {
+            //Outside shadow
+            Debug.DrawRay(ray.origin, ray.direction*1000, Color.red);
+        }
+        
+    }
+}
