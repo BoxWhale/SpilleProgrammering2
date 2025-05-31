0b6d7b23dea0f23526851927433883e12d31f85a
Happy_Majima <62021893+BoxWhale@users.noreply.github.com>
2025-05-31 18:21:13 +0200

    BinaryStorage

--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -7,11 +7,26 @@ public class PlayerData
     public string username { get; set; }
     public int stage { get; set; }
 
+    [Ignore] // Scene number is stored in the upper 16 bits of stage
+    public int SceneNumber
+    {
+        get => stage >> 16;
+        set => stage = (value << 16) | (LevelNumber);
+    }
+
+    [Ignore] // Level number is stored in the lower 16 bits of stage
+    public int LevelNumber
+    {
+        get => stage & 0xFFFF;
+        set => stage = (SceneNumber << 16) | (value & 0xFFFF);
+    }
+
     public PlayerData() { }
 
     public PlayerData(string username)
     {
         this.username = username;
-        stage = 1;
+        SceneNumber = 1;
+        LevelNumber = 1;
     }
 }
